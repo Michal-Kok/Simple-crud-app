b2d8c127c85ad80ccd1f5b9bf201fa3e72c41222
Michal-Kok <pukkix17@gmail.com>
2021-03-31 13:34:31 +0200

    svg added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../assets/style/Footer.scss';
+import { ReactComponent as Logo } from '../assets/image/Logo.svg';
 
 const Footer = () => {
     return (
         <div className="footer">
-            <p>Hello, i'm footer!</p>
+            <p className="footer__text">
+                Powered by:
+            </p>
+            <div className="footer__svgContainer">
+                < Logo />
+            </div>
         </div>
     );
 }
